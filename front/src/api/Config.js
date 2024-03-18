3264f82320d500359dc567db30fe4d25fe1d41c7
HJ-dp <skwo777@naver.com>
2024-03-18 14:51:22 +0900

    build: msw 설정
    #S10P22B109-151

--- front/src/api/Config.js
+++ front/src/api/Config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5db60f69727e2e81618ca4d003716b31de9fda0c682e5f6a489beea559766103
-size 271
+oid sha256:d78ab4cefd76a716beabcb5798d61775df4c5499d74d14493f15308663a6a68c
+size 1163
--- front/src/mocks/handlers.js
+++ front/src/mocks/handlers.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ffb413f9c6c6dc9f22a04e4e62c889d8839747e12152ed8c8398526f1383f14
-size 332
+oid sha256:03b2ea8581992aad9af746fb72cc73ac633ff50977721a46945363a8b6fa8a91
+size 354
