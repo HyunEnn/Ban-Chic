77b1340e35a3ab4722f11fa3fc131f90b1776497
HJ-dp <skwo777@naver.com>
2024-03-18 14:51:22 +0900

    build: msw 설정
    #S10P22B109-151

---
+++ front/public/mockServiceWorker.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bca11049c6082d09cbe54f5c74f70e319077444be0c0584040a4b6bf4c618e10
+size 7672
---
+++ front/src/mocks/worker.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb12fc84f921ddd75428d5f44a591beea7b34349c1c20ec77fa6cba62a651772
+size 131
