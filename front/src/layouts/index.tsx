6e480fa78b2dedbaee75ad0794d14f3049f11b93
이호진 <skwo777@naver.com>
2024-03-19 09:55:41 +0900

    style: Router 수정

--- front/src/layouts/index.tsx
+++ front/src/layouts/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9f4b15d3d1ba0daf3b057c814b8869ae1837d127107482911a4ec6b2f59b834
-size 634
+oid sha256:694173fa8c31d70cdf0bf973245e575ec195552d8e7cbc186b292d33a2f4d2c9
+size 692
