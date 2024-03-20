9059ff3229de7230310dc1e1a7e4df35591848b2
김성재 <sngjae972@gmail.com>
2024-03-20 09:55:48 +0900

    fix: 미사용 엔터티 제거

--- back/src/main/java/com/ssafy/banchic/domain/entity/Brand.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8e72b4347a446e8fdc00fbaa9749e5172d810b6daa7bf4dbeb57f0e7f1bc038c
-size 601
--- back/src/main/java/com/ssafy/banchic/domain/entity/Member.java
+++ back/src/main/java/com/ssafy/banchic/domain/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cae3c6e99f0b31ae2b251881ad1750c974e557e0232ddaa73ea9a26c30db5f6
-size 892
+oid sha256:54b3b96770f18b226a6d09913305e6e65e2e0dbe4006281df8054c1279993210
+size 691
