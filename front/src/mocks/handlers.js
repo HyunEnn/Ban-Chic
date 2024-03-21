71253a6d511007f2e77547d13d11670cd0ce2409
이호진 <skwo777@naver.com>
2024-03-21 13:26:28 +0900

    feat: 향수 상세 조회 API mocking
    #S10P22B109-151

--- front/src/mocks/handlers.js
+++ front/src/mocks/handlers.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03b2ea8581992aad9af746fb72cc73ac633ff50977721a46945363a8b6fa8a91
-size 354
+oid sha256:b982dff2917b86eca733deb671a83fbbb7ac8cdb16f5ee43ed806160d553c3de
+size 2220
