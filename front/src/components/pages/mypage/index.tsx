85f9d169090792d7ba4cbb77931e9b2629f00ac4
이호진 <skwo777@naver.com>
2024-03-21 14:01:50 +0900

    fix: stash 삭제
    Former-commit-id: a8f15145fa68dd8203ef481b3ed856d94e7d3652

--- front/src/components/pages/mypage/index.tsx
+++ front/src/components/pages/mypage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5732339c61c38282fdbbfd58604ccb19b5a581a896dba6cca793069c6c23106
-size 3919
+oid sha256:a62bfc258379985b46dd4c926961fc419aab6d00560b240935dd01d1b37afb82
+size 3341
