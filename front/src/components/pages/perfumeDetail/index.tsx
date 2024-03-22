f1a432e2dd5175cb045b0bbf34af20d6724d4a68
구본민 <gugabm@naver.com>
2024-03-22 12:52:53 +0900

    feat: perfume detail page
    Former-commit-id: e5c74a0a20b4d206c95a275ddaba6188173473b5

--- front/src/components/pages/perfumeDetail/index.tsx
+++ front/src/components/pages/perfumeDetail/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3961e4d69db198870b4ed21e7372565c0e5f4559a1d1bc9377bdbc0b47417a61
-size 1199
+oid sha256:985acd7a14411ab75d7b67c14920de843926b03db5210b556d61d5d3d13a5764
+size 3256
