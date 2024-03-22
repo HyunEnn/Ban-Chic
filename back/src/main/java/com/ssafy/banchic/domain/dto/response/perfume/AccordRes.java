56319f06c76da2eed354c5f3f740c1819fcbdf44
김성재 <sngjae972@gmail.com>
2024-03-23 00:02:02 +0900

    fix: 향수 상세 조회 API 반환값에 accord 내용 추가

--- back/src/main/java/com/ssafy/banchic/domain/dto/response/perfume/AccordRes.java
+++ back/src/main/java/com/ssafy/banchic/domain/dto/response/perfume/AccordRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:644466d957169d7bf9555bdfca03baf6182977bab00fb9c9bdcf9f5e058837c3
-size 838
+oid sha256:94fce052d33af1d55adbe02b5488e26c75e9c45b71a533d3b3bb2cba771c28cd
+size 844
--- back/src/main/java/com/ssafy/banchic/domain/dto/response/perfume/PerfumeRes.java
+++ back/src/main/java/com/ssafy/banchic/domain/dto/response/perfume/PerfumeRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afe01c0ec8c1d1089f8026cba044484617ace2b3e633bd59c275a5fcce48640d
-size 1585
+oid sha256:f6fd6a15b0faf6af3dd3b1682ca70671c0c8a6bd322a5efc7d611168ab600a2b
+size 1642
