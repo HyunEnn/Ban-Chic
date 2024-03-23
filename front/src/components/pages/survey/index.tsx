0d5bb3964763b965086caebb688dbc17a52435e6
이호진 <skwo777@naver.com>
2024-03-23 20:40:49 +0900

    design: 설문 페이지 퍼블리싱
    #S10P22B109-181
    
    
    Former-commit-id: 3fe86a2bbdc6493a58429ad1a88ede09baed76c2

---
+++ front/src/components/pages/survey/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee57907bb7e62fe57e2954b23da68a5a7f70261ba459b60e657027fa7630b3dd
+size 1614
---
+++ front/src/utils/PursuitBeauty.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1cd06eac0f863c8dcbdebd091e2527daeff914b48872eede02ebfd93588bdab
+size 731
