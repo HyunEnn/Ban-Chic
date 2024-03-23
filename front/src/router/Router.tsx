d638de0bea3148226f700b81c855a959ebe3f590
이호진 <skwo777@naver.com>
2024-03-23 20:40:49 +0900

    design: 설문 페이지 퍼블리싱
    #S10P22B109-181
    
    
    Former-commit-id: 3fe86a2bbdc6493a58429ad1a88ede09baed76c2

--- front/src/router/Router.tsx
+++ front/src/router/Router.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc03ab2f3a66624e414dfe12902ba47fe3d2c325b2cfa3cd6a441f51b130cbee
-size 791
+oid sha256:69896a51c2b50db550f19d1cd37f4b88b07c59fe9030668bc22bd6515031b9e9
+size 908
