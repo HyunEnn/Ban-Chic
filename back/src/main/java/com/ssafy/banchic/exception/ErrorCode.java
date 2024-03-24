8917fc2e9d0a9d3c68bd9b24adf76efdc1438a52
김성재 <sngjae972@gmail.com>
2024-03-25 00:26:08 +0900

    refactor: 스프링 시큐리티 최초 설정 & RefreshToken 엔티티 추가
    Former-commit-id: 98acb8fd76f7bcffb966fbe802eb5d767fe3695a

--- back/src/main/java/com/ssafy/banchic/exception/ErrorCode.java
+++ back/src/main/java/com/ssafy/banchic/exception/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76d05bef273a9b642e6363e8306df5d329a274ddf29d99b7073b57fc1c090fa0
-size 637
+oid sha256:6d6ae00c082c75aabe1ae54c20e05deae287c4bbfaadeb2f382eb1f1e74dd42e
+size 831
--- back/src/main/java/com/ssafy/banchic/service/OAuthLoginService.java
+++ back/src/main/java/com/ssafy/banchic/service/OAuthLoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fc82b810bc2cb8c35ea13ff5546d5a2dfb3d2982ad4027cc4909654464acb90
-size 5835
+oid sha256:36335b38bb1f6c588d85d504fbbd135604c327d616c4432ac0b5a0cbab2a6a78
+size 4091
