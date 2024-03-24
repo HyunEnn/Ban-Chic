67e23c0697ac82f1b08d428e74bdfa227bf6bc18
김성재 <sngjae972@gmail.com>
2024-03-24 22:02:59 +0900

    fix: CORS 헤더 노출 설정
    [- 토큰에 대한 노출 설정]
    
    
    Former-commit-id: a28202d605fb7fbe5bfabf116fffed3839d0f38f

--- back/src/main/java/com/ssafy/banchic/config/WebConfig.java
+++ back/src/main/java/com/ssafy/banchic/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:119ad442b26e069f1793db2523ef620cc05c77ed5ad1e17b6a6528196ed026c5
-size 540
+oid sha256:3ea6b9b5c76ef53667b8591c1c163c83d5da872d7a1d9f45ebb55838afb1e331
+size 601
