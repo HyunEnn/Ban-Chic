044949fd0a66c66c38db17514d8493214a456fae
김성재 <sngjae972@gmail.com>
2024-03-24 21:35:36 +0900

    refactor: 소셜로그인 API 로직 수정
    [- 불필요 클래스(OAuthLoginParams와 그 구현체) 제거]

--- back/src/main/java/com/ssafy/banchic/oauthApi/params/KakaoLoginParams.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:20f160e71bcfdd661ef39d4a6ca2b9becb21d516f0a2d9453f328b4ac9c1cb01
-size 342
--- back/src/main/java/com/ssafy/banchic/oauthApi/params/OAuthLoginParams.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:82e4ec16d882f2482102b3fe4ead77d06559f55abbb944f0b86e44d3fa032104
-size 387
