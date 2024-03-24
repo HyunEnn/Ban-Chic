823bb9d62456f86d0a1a6bd145a4fcbe57d04336
김현준 <denny10002@naver.com>
2024-03-24 21:27:00 +0900

    feat: 마이페이지 맴버 정보 조회, 유저 삭제

--- back/src/main/java/com/ssafy/banchic/config/JwtAuthenticationFilter.java
+++ back/src/main/java/com/ssafy/banchic/config/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:055dd060804b11bba455f110abdd958ad0fec504e010025ddae39bb50e4ff7a9
-size 2660
+oid sha256:0a696b381c10fb55b471c905092f85f6bc31a660c7c89af3049a5d00346d7a31
+size 2925
