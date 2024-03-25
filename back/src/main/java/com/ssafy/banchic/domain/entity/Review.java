a31f62b3300b0685ee86fabe7997004286b83864
김성재 <sngjae972@gmail.com>
2024-03-25 17:56:28 +0900

    feat: 리뷰 목록 조회 기능 init
    Former-commit-id: 3cb80e411ced30e0b33bd257a83548ebeb6c1205

---
+++ back/src/main/java/com/ssafy/banchic/domain/entity/Review.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa52590b10cd44f0ac62730f60cdbe91645e106727e199d963fd4921ddaec81e
+size 1090
---
+++ back/src/main/java/com/ssafy/banchic/repository/PerfumeReviewRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:052dea661b8ddfabfd5722666e655292b2da9b468092036b77b339d8a5e54f53
+size 380
