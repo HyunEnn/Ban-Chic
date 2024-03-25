b3e73f081d3a8331bddb389db00208c71790db77
김성재 <sngjae972@gmail.com>
2024-03-25 16:42:58 +0900

    feat: 멤버 이미지 수정 기능 init
    Former-commit-id: 6359d0ed1e4c4388cecc0d0ca2c4ecc26e5ce564

--- back/src/main/java/com/ssafy/banchic/service/MemberService.java
+++ back/src/main/java/com/ssafy/banchic/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d3d95a259cca57139136cefe5bf10b6b76fd88ad794e7a0c6d8f6f713877531
-size 1780
+oid sha256:cb0bdb3cdccc3a2855b336609f91a1decd709030b9c510121fbc8d66fca5b9ab
+size 2494
