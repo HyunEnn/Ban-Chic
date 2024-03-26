d9c0a8315848f534e56f12966465874686e1d09b
김현준 <denny10002@naver.com>
2024-03-26 10:52:10 +0900

    fix: 에러 코드 수정
    Former-commit-id: c866a879d2041b3067824a338ce5cedc33d30fbb

--- back/src/main/java/com/ssafy/banchic/service/HeartService.java
+++ back/src/main/java/com/ssafy/banchic/service/HeartService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f42bcec8400f360f3c0d823e5cd55cf9e406a5d4d5fc470421da88d4dce4a972
-size 2137
+oid sha256:73532298d1a625b5bf978fbaa130a9bd6e75fe6563a04d4fbe49e2c0020f721d
+size 2139
