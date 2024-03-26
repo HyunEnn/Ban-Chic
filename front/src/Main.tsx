f00fa3c03c4c5fc38e1b89e93a71f724fdab3aad
이호진 <skwo777@naver.com>
2024-03-27 03:59:38 +0900

    fix: msw 오류 수정
    Former-commit-id: 00a2e2acf8a1bc1414566a983d7d49e91f6abec7

--- front/src/Main.tsx
+++ front/src/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c912fd7d189b09c55e2cadae59dbff511eac8194c6669be976d614e4dc5b29a5
-size 813
+oid sha256:6e6efa6d5b7ef8304de451756228cb14a23c37569c829dce3d58eb68c0adc713
+size 853
