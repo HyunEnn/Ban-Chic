7dd562543b60bade20f48d4a8426dab52a709100
김현준 <denny10002@naver.com>
2024-03-26 10:30:50 +0900

    fix: 좋아요 기능 관련 기능 내용 수정
    Former-commit-id: e2ac742d7b25e76d91445150f69949fb3ee98243

--- back/src/main/java/com/ssafy/banchic/domain/entity/Perfume.java
+++ back/src/main/java/com/ssafy/banchic/domain/entity/Perfume.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1be19291dd4554dc51bcc160c330e7b87e6e38c73f9137b3b960b9262a0a813
-size 2180
+oid sha256:2b141b6f947ffc20fb44a26ce5c216691c392c82c8e33a99725db1ae3fd3190c
+size 2103
--- back/src/main/java/com/ssafy/banchic/exception/ErrorCode.java
+++ back/src/main/java/com/ssafy/banchic/exception/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22de01d8dec0f9234c1ec7526e1fcdfcef83678db1bca4fc6c7e98a27fc8a9e4
-size 1092
+oid sha256:e6ea05ed1082b39585394dab12e5442bd9b3df3ccb4efbeb9b3279fb2120e964
+size 1160
