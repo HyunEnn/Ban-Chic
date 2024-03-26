93dc0a4d867e7c7baa629e74b350da5d850389a0
구본민 <gugabm@naver.com>
2024-03-26 16:22:41 +0900

    feat: sidebar init과 향수 상세 api 연동
    Former-commit-id: f3743ba6b85baf64d27ba6c3193782ed3c677eaa

---
+++ front/src/components/atoms/menuToggle/menuToggle.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3cde6380b9f67b8768a7a6a48195f63d4a1e9a441b9bf7f728e22471ddee2fde
+size 1437
--- front/src/components/pages/main/index.tsx
+++ front/src/components/pages/main/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f0db1af0b932505ff2dbcd8fc096fc021207ed55b6b01cddbeaf7c88cad93ad
-size 7203
+oid sha256:a585d01baf86fb22b4cc2842c52309ad1d08e03186e0bb825b7e558760f2dc03
+size 7073
