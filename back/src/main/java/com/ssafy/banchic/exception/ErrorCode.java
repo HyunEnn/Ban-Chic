6606cff63a9d14fc16eda72eacb4d187479aafe2
김성재 <sngjae972@gmail.com>
2024-03-26 09:57:58 +0900

    feat: 리뷰 수정 기능 개발
    Former-commit-id: 85f9595249d563f0a16ba1a1be92954594a1500a

--- back/src/main/java/com/ssafy/banchic/exception/ErrorCode.java
+++ back/src/main/java/com/ssafy/banchic/exception/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22de01d8dec0f9234c1ec7526e1fcdfcef83678db1bca4fc6c7e98a27fc8a9e4
-size 1092
+oid sha256:0f6b9fa4e682494b8bbb9a52a9bbc9a62100546e485242de837a58ab430b8693
+size 1181
