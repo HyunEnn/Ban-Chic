740166a268c65e785d8f15e3cdcdf9a1263a5af2
김현준 <denny10002@naver.com>
2024-03-26 11:00:47 +0900

    refactor: 멤버 메서드 수정
    Former-commit-id: 2b306b5387d4db765a568062c7b1b022717a25c0

--- back/src/main/java/com/ssafy/banchic/service/MemberService.java
+++ back/src/main/java/com/ssafy/banchic/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:716a686e69bc403c2b711850e39cf64987aca0922bee9b51adbef636bc3cdb52
-size 3552
+oid sha256:851cbead1b512100447acdb65c941c5c0a8d7358811c0f71913a0ef309da1899
+size 3283
