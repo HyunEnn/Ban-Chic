1d8dd1f2e589af350da1b914f9c0ec850eb263f6
이호진 <skwo777@naver.com>
2024-03-27 09:33:02 +0900

    design: framer-motion 적용
    #S10P22B109-227
    
    
    Former-commit-id: 86ac097cc92d889b3a6d893671164ffeea1caee7

--- front/src/components/pages/login/index.tsx
+++ front/src/components/pages/login/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a37e5d1eab9a0239dc29c1e824b4934cd09ef73b636ef44353d60de93b954004
-size 1136
+oid sha256:1470a4d3a56240aa8f2b9158770a532c97c831ac84b835187e781cf7db5d350e
+size 1455
