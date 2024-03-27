c1aae1e83ca6d5db23009fb5c0a9fd8147a2ce3a
엄진식 <ladiescheong@naver.com>
2024-03-27 14:51:43 +0900

    feat: dockerfile 및 이미지 작성, 수동 배포 완료

--- ai/fastapi/recommend.py
+++ ai/fastapi/recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c78d63aca2a89ca4e22d9bddf15e780ab051d4b6b608f400382625db8d34fe46
-size 1291
+oid sha256:7773d2f12eca0f4c41652308981d05f785b9e5388d44e514f2eaeefdcd2caa4a
+size 1289
