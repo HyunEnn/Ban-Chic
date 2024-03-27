e2de8a004f4f77665c321b9a13fbb2b63b1a96f5
이호진 <skwo777@naver.com>
2024-03-27 10:15:11 +0900

    feat: 프로필 이미지 수정 구현
    #S10P22B109-225
    
    
    Former-commit-id: d2d7f402437bc94a7195c83f92adfef9dfaadc74

--- front/src/api/Api.tsx
+++ front/src/api/Api.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15fd83a6cc22341a5512781e525547e71fd36e338acfbd83b17008cb59c67f5c
-size 2937
+oid sha256:5d69cc9dba67f38d456583eba54b6677f80a9377d4ca6a502a88eed9adc25cbe
+size 3092
