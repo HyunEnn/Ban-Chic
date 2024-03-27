460a2f347ddc87ea87daf9d68ddecf015ddee309
엄진식 <ladiescheong@naver.com>
2024-03-27 16:49:26 +0900

    feat: user_id 요소를 제거하도록 API 수정

--- ai/fastapi/main.py
+++ ai/fastapi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b0ca15f0c7c2ced09698bd5c6e8f76e302cc2049ce37541a02aafc181fe3ada
-size 628
+oid sha256:9802915c62723356510ec125d7b0734d2214789501004526efa0d0c4aa40ad56
+size 611
--- ai/fastapi/recommend.py
+++ ai/fastapi/recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7773d2f12eca0f4c41652308981d05f785b9e5388d44e514f2eaeefdcd2caa4a
-size 1289
+oid sha256:79a17e27f277dd74f47c2043f7deda8d8bfc2c0cc0406ff2b1624f5f4566ae02
+size 1149
