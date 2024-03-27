1479180adc67930ecfb66d596fe49bc32f695559
김성재 <sngjae972@gmail.com>
2024-03-27 15:05:18 +0900

    fix: 토큰 갱신 로직 수정
    [- request header에 refresh token는 것으로 변경]
    
    
    Former-commit-id: 594bc8ccc472129ed4919e192557190bba061132

--- back/src/main/java/com/ssafy/banchic/service/OAuthLoginService.java
+++ back/src/main/java/com/ssafy/banchic/service/OAuthLoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fdee51eb63d8a5ffb847b5d73775c0e33fd60b3d24b2b9f6f92e145bb1685ce
-size 4374
+oid sha256:66806e24e6381bb7e12b545e4a3d5be5d1c3d5d825d010f8a1f69fe6b142d576
+size 4867
