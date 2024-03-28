bc394ca29f193c427131c9496818eefddb3f2594
김성재 <sngjae972@gmail.com>
2024-03-28 22:21:54 +0900

    fix: 미사용 클래스 제거
    Former-commit-id: 255be2840d1d5a8daa724c0d504843351445d453

--- back/src/main/java/com/ssafy/banchic/repository/perfume/AccordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.banchic.repository.perfume;
-
-import com.ssafy.banchic.domain.entity.Accord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccordRepository extends JpaRepository<Accord, Long> {
-
-}
