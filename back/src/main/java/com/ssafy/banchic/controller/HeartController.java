dbf5329a59c441a539c76e07be091f88ef7ec8e0
김성재 <sngjae972@gmail.com>
2024-03-28 21:32:42 +0900

    feat: 추천받은 향수 목록 조회 API 개발
    Former-commit-id: 81b193fbd4dfa9a6c4a6b087df64273d8ab95b39

--- back/src/main/java/com/ssafy/banchic/controller/HeartController.java
+++ back/src/main/java/com/ssafy/banchic/controller/HeartController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afbea52be805ba21df173f2cfcf7496377d13ee135ff07aa4ae350ec82229c5b
-size 1879
+oid sha256:be6c000443e30059c9f3ebe54618114db3297c10a1123c15552ea1cb37ec0310
+size 1999
--- back/src/main/java/com/ssafy/banchic/domain/dto/response/PerfumeOverviewRes.java
+++ back/src/main/java/com/ssafy/banchic/domain/dto/response/PerfumeOverviewRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:576d846e5421815ed6b063fb785723f4a465279fa353d2cb1ba3d46af56716b8
-size 564
+oid sha256:59247684e174a17622496c68bf5dd3953e6790fce8d88dfbcf7d93e273773cda
+size 567
