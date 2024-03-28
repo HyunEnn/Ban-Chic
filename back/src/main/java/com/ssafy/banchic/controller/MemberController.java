e18d2ab66114605ff12c2756b2e5a078a26d3efd
김현준 <denny10002@naver.com>
2024-03-28 13:01:19 +0900

    fix: 맴버가 좋아요한 향수 목록 조회
    Former-commit-id: 6a9cb4bbfa3fe973d2ab27b14b9c1cf0234e687f

--- back/src/main/java/com/ssafy/banchic/controller/MemberController.java
+++ back/src/main/java/com/ssafy/banchic/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a16829517fb7f5bd344a660f8116a2d2dd3c9de7df3eea3332b4be2882e0ba6a
-size 2959
+oid sha256:9c51c12fa733c5152535b3f1ef850d1bff79f4a470a51dfd97a39e54afa125a3
+size 3355
