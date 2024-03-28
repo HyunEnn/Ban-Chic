72d04d1965bc4ac910872f0c0101817112352cf0
김현준 <denny10002@naver.com>
2024-03-28 22:28:46 +0900

    refactor: swagger Tag 수정
    Former-commit-id: 6126246c97ddaab0da0e10e1fb998a245db2abfa

--- back/src/main/java/com/ssafy/banchic/controller/AuthController.java
+++ back/src/main/java/com/ssafy/banchic/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b795af0d646329ee3ae9187c563c47ad31fea0287ef6108c3dff3fc71ca34ab
-size 6155
+oid sha256:05df2b8f93b62444491b95ae7ef97b119ff5d6cdb9e9d1efe1a34ec79e33de8c
+size 6151
--- back/src/main/java/com/ssafy/banchic/controller/HeartController.java
+++ back/src/main/java/com/ssafy/banchic/controller/HeartController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f571c9e241a21097a8ec1fdb15a5a8a29212c142fc82adb2d9a1b17de6c3517
-size 2669
+oid sha256:6bd70f8505fe53f0c224b6c53a71416ac282c14e6205c3937184d7a6bca764b5
+size 2658
