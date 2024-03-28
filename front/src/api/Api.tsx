e1fc5f70fa9f72310aa12c1051f89f12867ee723
이호진 <skwo777@naver.com>
2024-03-28 11:16:32 +0900

    fix: API 재 수정
    #S10P22B109-225
    
    
    Former-commit-id: 42f8ecbe2aad4df9500dc4c194c05ed5945e5eae

--- front/src/api/Api.tsx
+++ front/src/api/Api.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbfaac104edc1ab7b220b3f6e2cf3fe1f403ea881ad943c9e1d753f66124adc8
-size 2848
+oid sha256:38816c12a851c1b50cbebe249d7fe10c8f35aeb85e1e4afa83770c57b0428477
+size 2874
