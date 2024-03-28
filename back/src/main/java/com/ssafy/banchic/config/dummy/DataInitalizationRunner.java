71ed112909bc12a10ac04d75c759219e4b867f3b
김현준 <denny10002@naver.com>
2024-03-28 22:31:55 +0900

    hotfix: component 수정
    Former-commit-id: cb3b7a05ad3ce007ff32add71e3e52c5461ec4ff

--- back/src/main/java/com/ssafy/banchic/config/dummy/DataInitalizationRunner.java
+++ back/src/main/java/com/ssafy/banchic/config/dummy/DataInitalizationRunner.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36f9f347fe8be11d5d58b75c9e6b761156cb5ebccee6eb046b2bff8018d7fca6
-size 949
+oid sha256:b36305ee44891403ec725e113e5652324f338478edab49c77c3011acb2600b39
+size 951
