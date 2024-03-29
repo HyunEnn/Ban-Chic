cf2405881e32afb1415b7919fcd913446781f88a
이호진 <skwo777@naver.com>
2024-03-29 10:46:04 +0900

    fix: 콘솔 에러 수정
    Former-commit-id: c8bb9a2e205419885d14de8a52a679d066a152d6

--- front/src/components/molecules/list/circleItemList.tsx
+++ front/src/components/molecules/list/circleItemList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:add4565ebd5cc42b880d9c79c59734ca1269e1badb423d8771aed63f729460ee
-size 783
+oid sha256:466ea2bcc17e317ed62837164099385d74e229e2f5686d3c611ace0f6af12ef0
+size 808
