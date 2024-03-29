78d8229d035410a57fba2a5412d5b1c830dc1f51
구본민 <gugabm@naver.com>
2024-03-30 01:12:32 +0900

    feat: sidebar 완료
    Former-commit-id: 1dcf6c6cea494d4fa6acf3d3f346fdf906d47c82

--- front/src/components/molecules/common/gnb/index.tsx
+++ front/src/components/molecules/common/gnb/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21c2e56ed2fbb75d92b2e8cc0ee3f5a0bf104c4a665ec15d99713c10aa8afc9c
-size 2247
+oid sha256:cf111bb37ee99d3c6794d30b8c181df67a7b1abba50074c0127b80b2fabb7a3d
+size 2868
