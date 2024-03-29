205eb2d182f059f92346760d82cd58787472b01a
김성재 <sngjae972@gmail.com>
2024-03-29 15:07:07 +0900

    refactor: 리뷰 등록 API consume 설정
    Former-commit-id: 5255428b70523269d161779e733e04cd26c6036e

--- back/src/main/java/com/ssafy/banchic/controller/PerfumeController.java
+++ back/src/main/java/com/ssafy/banchic/controller/PerfumeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:823335b3c9b0745ba7f145e8b7e4f7e19e303ff08f3bd3239e90ae163ae4f8fb
-size 5257
+oid sha256:9d9e3d1871f2c4842d56afd7ca9be0dfb85f0527baf04a256de1333701369a84
+size 5392
