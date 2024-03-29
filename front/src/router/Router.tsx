bd5f8b3c89fc5bd542cd076cd3ae0736c6073edd
이호진 <skwo777@naver.com>
2024-03-29 10:16:43 +0900

    refactor: 동적라우팅 적용
    #S10P22B109-231
    
    
    Former-commit-id: 316c72bcbfad7a6eafb99f6f6e553bca62bda7f2

--- front/src/router/Router.tsx
+++ front/src/router/Router.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c019761c0efdcc6ec659b05a7051d1639529fc8050ca549e6f9f4670b80c14e5
-size 1716
+oid sha256:3e1abdb72974afb0c0f883a6f9401fb0c9a51121d15a089d9df2a531ab22d873
+size 2624
