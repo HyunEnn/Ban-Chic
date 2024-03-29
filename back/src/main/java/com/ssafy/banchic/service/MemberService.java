248da25ddc008e2ff27f72266c0f83e4eece2da2
김성재 <sngjae972@gmail.com>
2024-03-29 16:26:12 +0900

    refactor: 내가 작성한 리뷰 목록 조회 API 반환 양식 수정
    Former-commit-id: ac48535459f67dc51f055fe500ec675eaa71c028

--- back/src/main/java/com/ssafy/banchic/service/MemberService.java
+++ back/src/main/java/com/ssafy/banchic/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5df2c302028329fdecedff4cf9039b9c707906f6dfc5199d8ddd80f02041d5e
-size 11845
+oid sha256:5d83c118cd171f0f337400f71fffa22bf64871ae07814534df79c528f7f870e9
+size 11862
