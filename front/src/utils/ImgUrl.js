a5a84890aebb6c7b8f3c37d4ecdd5f6c77ebc1dc
이호진 <skwo777@naver.com>
2024-03-30 01:16:42 +0900

    refactor: 동적 라우팅 설정
    Former-commit-id: 113d21c4f1de59cb64545225612b76ea9c059067

--- front/src/utils/ImgUrl.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:feafd3edb54cf2dc2de39470cc8ce65fc4d6f4fd6a431fc0f4e6be6260ce9968
-size 109164
---
+++ front/src/utils/ImgUrl.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0865716a08a3e4e0b97c4ea418a4569abf72e843e80a4681318937c9c5ec904a
+size 106222
