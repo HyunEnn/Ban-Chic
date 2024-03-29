c1420956df35d2b244bc827af961b5c13476bb27
이호진 <skwo777@naver.com>
2024-03-30 01:16:29 +0900

    feat: 추구미 불러오기 연동
    Former-commit-id: 7823682e770ccdaab91cbc14005227581cd52125

--- front/src/api/Api.tsx
+++ front/src/api/Api.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3796a0f69a2f8d864afa43e1c65f204b7b3a71650857f463b0b0395defe1da83
-size 3113
+oid sha256:6a7980fe7ff43011eedcdbc026e4891a143213745f57b9f1b18185135dc56550
+size 3201
--- front/src/styles/Font.js
+++ front/src/styles/Font.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:038dedd0aea9b99c2c7a07eb416ba1d9cbc0c1360480d828fdce04512b2d89fe
-size 1184
+oid sha256:34d71c5dfcdca5280b119ea719d378016300c3df760e999be4a6adc4be8a35ba
+size 1542
