82f00019bf83c6b9de2a87b5f148664dacf288b8
구본민 <gugabm@naver.com>
2024-03-29 13:18:13 +0900

    fix: 리뷰 crud api 연결수정
    Former-commit-id: 3f7bc553481485068fd6b5874a7c252c22f3e22e

--- front/src/api/Api.tsx
+++ front/src/api/Api.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58bf8bcff2f8a0b73364d2abab94f96d273344656b685fc9af1423ad89509c78
-size 3421
+oid sha256:909b83376c4b72657609384bf1ea6a18a4962e9f742786563ac9c17dd4b35141
+size 4070
--- front/src/components/pages/crudTest/index.tsx
+++ front/src/components/pages/crudTest/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccbd600c0b2849839878df2506e396fed58322c16ebf1463f276dcb6eecf1059
-size 3156
+oid sha256:7e3f8b671c55760cacac26188fc12e61e5a61b9e929da2e5e20864276e392801
+size 3660
