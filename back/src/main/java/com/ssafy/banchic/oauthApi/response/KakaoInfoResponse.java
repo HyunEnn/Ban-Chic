1448b3f0ec36e388ce76e03c4b2730414169ee7a
김현준 <denny10002@naver.com>
2024-03-29 14:14:23 +0900

    feat: csv 한글 네이밍 추가
    Former-commit-id: 0e3e8b393bf1469ec8b5bc02b10cff84331aac6e

--- back/src/main/java/com/ssafy/banchic/oauthApi/response/KakaoInfoResponse.java
+++ back/src/main/java/com/ssafy/banchic/oauthApi/response/KakaoInfoResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1490cfc39d9063d5de94384aa58b82037082ae52c17df6095ae3251420a2134a
-size 1114
+oid sha256:e97deaae549d0249055dc34bcfb7e1c0a83e2ab3b717ac294e15fa6633c6737e
+size 1141
