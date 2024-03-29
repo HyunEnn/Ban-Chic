27d8e3e85ba9031bc4d1fb2019926c346bbd4c57
김성재 <sngjae972@gmail.com>
2024-03-29 10:52:56 +0900

    feat: 추구미 타입 조회 API 개발
    Former-commit-id: dfa096ea13a5d7143aa5445d2135f6fc2d503868

--- back/src/main/java/com/ssafy/banchic/controller/MemberController.java
+++ back/src/main/java/com/ssafy/banchic/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77bfc908f33c4355d3be0838b9876d8cb6046c17fa7530ed0014420981cd859f
-size 7379
+oid sha256:d58c7bbc593b5c66ec920be59ddbd19a04f110670d0e7e136b0a081a7dbe6348
+size 8000
--- back/src/main/java/com/ssafy/banchic/service/MemberService.java
+++ back/src/main/java/com/ssafy/banchic/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d26363ef722a231262fc64390b485bcec8f085296b4b9c1627b8185637013fa1
-size 10768
+oid sha256:2a8b17d0c34513fd53066385fabc3e6f7adabdf81897fb68a22b7fc92b3a5354
+size 11752
