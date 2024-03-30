c53838f3d19860a366f97749a740fc52389697a9
이호진 <skwo777@naver.com>
2024-03-30 14:57:32 +0900

    design: 기본 이미지 수정
    #S10P22B109-178
    
    
    Former-commit-id: 19936b00d974845feae3da9306e74743f3287b5b

--- front/src/components/molecules/mypage/profileCard.tsx
+++ front/src/components/molecules/mypage/profileCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65fd9f8cabcad5acf53ff9247865bd320a045ae4657f438e213f687c32344ce2
-size 1606
+oid sha256:faa4bf515482314d27e9e14d69fc2068caf083d822c5f1e21519f63addcd6ec8
+size 1522
