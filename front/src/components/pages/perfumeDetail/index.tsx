1d6608f95692b60ea4b9dfb463cb7969a1f3ad5e
이호진 <skwo777@naver.com>
2024-04-01 03:53:05 +0900

    refactor: 향수 상세 페이지 반응형 적용
    Former-commit-id: aacc1bf9d3f6b4fd5f49d0d929e4a2d96b74eee2

--- front/src/components/pages/perfumeDetail/index.tsx
+++ front/src/components/pages/perfumeDetail/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17b5735e4127b2c39c8816b0cd468bd601988838f24a222aa8491ac6db449c58
-size 7103
+oid sha256:753bf7ee466a4b55731bf8d2a7d2bc0c6b5d59c65993a69f3559be25f32031c0
+size 7265
