8c804bba6cf5bb44c4e266d9bcf6ddc1f23d2def
이호진 <skwo777@naver.com>
2024-04-01 03:53:48 +0900

    refactor: 사이드바 반응형 리팩토링
    Former-commit-id: 876f3fd588b5c9bb849614d18af698751f83737b

--- front/src/components/molecules/common/gnb/GNB.styles.ts
+++ front/src/components/molecules/common/gnb/GNB.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2352bab2e4376b09b72a11deca9760dd8aa435087f2e6a4d40c21f9c602e7ebc
-size 711
+oid sha256:982aa2ee7dc2aa5bf218a153cb2e6b36564d5babd597acf065472ae1c032181a
+size 811
