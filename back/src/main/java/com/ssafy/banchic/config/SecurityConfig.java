f2793c1f5ee19eb02188d76892b835c180b583ee
김성재 <sngjae972@gmail.com>
2024-04-01 01:35:26 +0900

    refactor: CORS 허용 헤더 특정
    Former-commit-id: aa8a4f05eca5cd58735db6222b450ea86d506a6d

--- back/src/main/java/com/ssafy/banchic/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/banchic/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d022c671063f3cf1ac875d6e1e6dcf9209c8836c7f55bcf662b1339ea95763c8
-size 4504
+oid sha256:9af4dee0b24b4fa3585945efac0a6e2c806545d624648ee9972fb46c55ef3f07
+size 4770
