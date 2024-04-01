d96720234327f8c1a455796bf6e788f1e81ce15f
구본민 <gugabm@naver.com>
2024-04-01 14:11:17 +0900

    refactor: gpt prompt 상세페이지 입장 시 자동 호출
    Former-commit-id: b6e1eb77ee28256e6262643f528b9eda0b077207

--- front/src/components/molecules/detail/noteGroup.tsx
+++ front/src/components/molecules/detail/noteGroup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cbb88d4d9212d699cc65e13ddb9ff2d239598895bf19ac8569fdd6c930c4d2d
-size 1167
+oid sha256:e72da92a90d49a1f16200b6cd7d35c5f16c9c794df2a69cde2a509b9579e8597
+size 1114
--- front/src/components/pages/crudTest/index.tsx
+++ front/src/components/pages/crudTest/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb35e33a5087a417f122d1e819446f7bee50b3db4f949bede964930738e198e6
-size 3514
+oid sha256:6a57b994a626f88ed5219afa8d6662478d0ab2a5c2b952e151a85af3179a494c
+size 3243
