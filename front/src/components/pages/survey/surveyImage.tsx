db5103b2425eed3d49a930f87688460c5c57acbf
이호진 <skwo777@naver.com>
2024-04-02 00:17:25 +0900

    fix: 빌드 에러 수정
    Former-commit-id: 955e4577b33b6dbc59b7a30d9c7defaa294cbc70

--- front/src/components/pages/survey/surveyImage.tsx
+++ front/src/components/pages/survey/surveyImage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d106b8c579b67c69e634a671ec81a88649bcd735085b669d1f0d3b30b2f5d42
-size 3944
+oid sha256:2e1bb2df0b44376cf857e05148d1e28ec00fdd7c00167a19eda23a1a16b157f3
+size 4254
--- front/src/components/pages/survey/surveyLanding.tsx
+++ front/src/components/pages/survey/surveyLanding.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f828311f48aeddbcc60f91a1212fa59f18fe9c36111cb059d5408f8a6acce7b
-size 4252
+oid sha256:eca28200752505818984c32289a949b6c41c8b0215f10173ba388c41182164bf
+size 4330
