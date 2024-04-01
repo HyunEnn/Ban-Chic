85ee4d4155f58820fb74e167857d9aa2590ae266
이호진 <skwo777@naver.com>
2024-04-02 00:42:24 +0900

    feat: 회원 탈퇴 기능
    Former-commit-id: 4fb1e8316cc99dfc8f69fafa0c02f7a68e34c47c

--- front/src/hooks/auth/useDeleteId.tsx
+++ front/src/hooks/auth/useDeleteId.tsx
@@ -1,12 +1,3 @@
-import { deleteMember } from "../../api/Api";
-
-function useDeleteId() {
-  const deleteId = () => {
-    deleteMember().then((res) => {
-      console.log(res);
-    });
-  };
-  return deleteId;
-}
-
-export default useDeleteId;
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c69ad690d6171b4c85beb25088942d8957ebb37f927dcb94efd3adb6cc850fa
+size 526
