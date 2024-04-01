1d4fd0e0554c11b620353220879172d1555dabdd
이호진 <skwo777@naver.com>
2024-04-01 11:36:12 +0900

    design: 사이드바 인터랙션 수정
    #S10P22B109-332
    
    
    Former-commit-id: cd9b2e6eb5d9c968c0317cb528e55add1a1811df

--- front/src/hooks/heart/useGetMyHeartList.tsx
+++ front/src/hooks/heart/useGetMyHeartList.tsx
@@ -1,9 +1,3 @@
-import { useQuery } from "@tanstack/react-query";
-import { getLikes } from "../../api/Api";
-
-export default function useGetMyHeartList() {
-  return useQuery({
-    queryKey: ["myheartlist"],
-    queryFn: () => getLikes(Number(localStorage.getItem("uid"))),
-  });
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:983a075e9fd36e6fddaaace53a70adc369da0fe3a4c3047875b638e7cbafd1ae
+size 264
