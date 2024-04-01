3e03787a5fd13ac1566ca7b9fe78d9bc9bbc3582
구본민 <gugabm@naver.com>
2024-04-01 20:36:10 +0900

    fix: gpt 문제 완료
    Former-commit-id: 665bd07450931bc42227eee654ed9b7461b6f476

--- front/src/components/molecules/detail/noteGroup.tsx
+++ front/src/components/molecules/detail/noteGroup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c82c3ae6a47162980e2e6fb25891fda5b3d289e4e6028d6c432d59a4278773f8
-size 1711
+oid sha256:72c59b0d212dc90a2c203ed8b2d16c7eac1704cfe85e1a73828b6fd498e7740f
+size 1617
