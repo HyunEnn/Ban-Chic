f89ce8f1b5bb4aadeb4e3ea93a4d960b0245749f
이호진 <skwo777@naver.com>
2024-04-02 02:33:30 +0900

    style: z-index 및 간격 수정
    Former-commit-id: 1407b01f7b30d0dadeff8b33c630a5f5acaefb95

--- front/src/components/pages/survey/surveyImage.tsx
+++ front/src/components/pages/survey/surveyImage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e1bb2df0b44376cf857e05148d1e28ec00fdd7c00167a19eda23a1a16b157f3
-size 4254
+oid sha256:7a7d4c3bc07d9835a599a8eea3c929e515344a2d5bffdab9ee6d03321832184b
+size 4302
