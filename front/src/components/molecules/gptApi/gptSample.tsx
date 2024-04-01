503f3752b594dd8b775ffa70cb28b9dd43034b56
구본민 <gugabm@naver.com>
2024-04-01 20:36:10 +0900

    fix: gpt 문제 완료
    Former-commit-id: 665bd07450931bc42227eee654ed9b7461b6f476

--- front/src/components/molecules/gptApi/gptSample.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1ee43d2a30756327db7c87905a7fa20857ca40fa0b1c45d63d92b7426021a3f1
-size 2391
--- front/src/components/pages/perfumeDetail/indexcopy.tsx
+++ front/src/components/pages/perfumeDetail/indexcopy.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dd80ea3d2af525439e6661728f3eb633009cda64391473449def77365dea4ce
-size 6999
+oid sha256:af1d86118a232a75bbb89a77528e2c8ecf1f195779930c9aebb29eba055d424e
+size 7966
