68ee898b022fb82d6e4a88b4eef5a64c27c9eb40
이호진 <skwo777@naver.com>
2024-04-02 00:42:12 +0900

    fix: 빌드 에러 수정
    Former-commit-id: 8442694985956860f9e0e0ed7b7681aa1f12bcd3

--- front/src/components/molecules/gptApi/gptWriter.tsx
+++ front/src/components/molecules/gptApi/gptWriter.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ff64113b2ae60335d2ff869833d67c1fd07bcbedf6992e65dfbab0bad14adae
-size 1221
+oid sha256:989ac35c1fc54d3de80496ddf03109927236d0a029027922fafc1923c49c8720
+size 1213
