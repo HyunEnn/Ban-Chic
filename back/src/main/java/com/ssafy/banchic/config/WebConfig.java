2579e9974701bbfb4ef99c737a52c5f91e73654a
김성재 <sngjae972@gmail.com>
2024-04-01 10:01:32 +0900

    fix: WebConfig에서 CORS 허용 URL 설정
    Former-commit-id: b16672f21f40767ab9312ea92661dcd64643482a

--- back/src/main/java/com/ssafy/banchic/config/WebConfig.java
+++ back/src/main/java/com/ssafy/banchic/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ea6b9b5c76ef53667b8591c1c163c83d5da872d7a1d9f45ebb55838afb1e331
-size 601
+oid sha256:dbcabf39d2c277f56d9ef093a30d8f14ca9b8b7602c15021a8df22dd175080b0
+size 621
