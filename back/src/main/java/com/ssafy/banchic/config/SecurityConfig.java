e71fb805b5ca6887ec8be134a0e90f0451d0bda6
김성재 <sngjae972@gmail.com>
2024-04-01 11:08:53 +0900

    refactor: SecurityConfig에서의 커스텀 CORS 설정
    Former-commit-id: d79be7ee10a81a7b143a29426b9e34bebbedd583

--- back/src/main/java/com/ssafy/banchic/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/banchic/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bec9e67f8d7de7820dc5ab6b27a356cff86e1777fa35b33ef8896f4e605d7590
-size 4496
+oid sha256:5dadfe54ca3a1f8709451cdac9832522159049f31bfab9c0a50a9220ce7a1c4b
+size 4548
--- back/src/main/java/com/ssafy/banchic/config/WebConfig.java
+++ back/src/main/java/com/ssafy/banchic/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbcabf39d2c277f56d9ef093a30d8f14ca9b8b7602c15021a8df22dd175080b0
-size 621
+oid sha256:29deb791c7399e043e59e05bf7266064145bea64fa86ee0658e687f18bc12bad
+size 657
