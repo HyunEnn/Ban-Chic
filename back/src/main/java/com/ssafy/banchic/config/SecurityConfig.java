c089763a3a0cd06cfd267799438226b72705c099
김성재 <sngjae972@gmail.com>
2024-04-01 12:49:07 +0900

    refactor: CORS 설정 위치를 Spring MVC로 이동
    Former-commit-id: 87e3a6ffdc1af0df61bc03e8134e07b3a2632778

--- back/src/main/java/com/ssafy/banchic/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/banchic/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dadfe54ca3a1f8709451cdac9832522159049f31bfab9c0a50a9220ce7a1c4b
-size 4548
+oid sha256:84767caf07a0fca6e959b05501a7e02bd93952614ef0cbe01d504c3a5a6e073c
+size 4321
