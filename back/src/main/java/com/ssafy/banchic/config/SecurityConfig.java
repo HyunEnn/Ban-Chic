cf2548407d5e83332a1640d72d7569b5190cd6a4
김성재 <sngjae972@gmail.com>
2024-04-01 12:51:16 +0900

    refactor: 미사용 코드 주석 처리
    Former-commit-id: 33f009292ef39930893a66ee6092df36a3107636

--- back/src/main/java/com/ssafy/banchic/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/banchic/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fa9c4c73f1f305331dae52336b17b01da89dae2feff3d40d6ab756adf5a354c
-size 4319
+oid sha256:84767caf07a0fca6e959b05501a7e02bd93952614ef0cbe01d504c3a5a6e073c
+size 4321
