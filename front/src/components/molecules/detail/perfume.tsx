39f20e5d75de8b026ce57e01535faf5e67b9a2ca
이호진 <skwo777@naver.com>
2024-04-02 12:27:12 +0900

    refactor: 변수명 수정
    Former-commit-id: 8ea028e73005f4a1925804ed65b924ef848f74b2

--- front/src/components/molecules/detail/perfume.tsx
+++ front/src/components/molecules/detail/perfume.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:148931b1f970f467a0261a758ef4956fa22c06cd6786333f476370b5ff8b9e6b
-size 2065
+oid sha256:282e217faa039ecc3e76a387eb8bef23bd013dfc01fc48bae693267e3647f716
+size 2079
