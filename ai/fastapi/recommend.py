e58553adef19c71fa64ce80c32deb176df104348
백성욱 <b9s2w6@naver.com>
2024-04-02 15:20:37 +0900

    ci: recommend.py 수정
    Former-commit-id: 81ac3536a11568a23df6f2fdde06ce8dba5da87f [formerly 727174b82289d5e6fa69e8566c3e2c0050ce8fa6]
    Former-commit-id: 99ed2881b9f7822786b9be3d39378e665e944dd7

--- ai/fastapi/recommend.py
+++ ai/fastapi/recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6f315ecbf30b90913f7905549e02bf71b19131f395adf0cb3e4f7c812dd0779
-size 6419
+oid sha256:21f49b733d9bfc890d6bc7e1320c7cf9a1207793b150df0b993a3d3adbaecfef
+size 6427
