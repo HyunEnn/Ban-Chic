2e4f2f1bbc6f53cd2efdf4680543f6b1279fd370
백성욱 <b9s2w6@naver.com>
2024-04-02 15:06:57 +0900

    ci: model관련 코드 수정
    Former-commit-id: 1e59d832b43810ed528400ed9e3776ad85fe4332 [formerly 13e9ffb52580152935ab25e9a9820bce9cef61ae]
    Former-commit-id: 245aedf7f4f4d3472d890c49f7b99d025c503bed

--- ai/fastapi/model.py
+++ ai/fastapi/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3aa2d6b57b743391473db20484677e4e968f6f399cfeec20c2ef8aff55571243
-size 1016
+oid sha256:9a2227dec5165e0aae22814571ccc49ad7fbe40d187a5b4724747294ad591439
+size 1007
