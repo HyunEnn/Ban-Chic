dc0911bab2c92e0fa1669bbc801dc5e7db10480b
이호진 <skwo777@naver.com>
2024-04-02 12:50:27 +0900

    fix: 빌드 에러 수정
    Former-commit-id: dfbc6cbd8084c7cdd357662a6de64bc0a939792f

--- front/src/components/pages/survey/surveyResult.tsx
+++ front/src/components/pages/survey/surveyResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f39f4baa5eedf793d440f95f95b354ab5c67b369509056f5f71fb3ba9bccf2be
-size 5075
+oid sha256:023016e17f9508fefa1dc0a042cb8eff82f402ed66e45d2bfe19eff009bcc624
+size 5017
