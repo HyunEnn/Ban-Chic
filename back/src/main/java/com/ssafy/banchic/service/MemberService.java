e2c0ec18608e66298d67ae3371b9d0081884d222
김성재 <sngjae972@gmail.com>
2024-04-03 00:47:31 +0900

    refactor: read 요청에 대한 Transaction readOnly=true 처리
    Former-commit-id: c318c6118e7ca3ce3b212bb9c03d3bbeb39c48fd

--- back/src/main/java/com/ssafy/banchic/service/MemberService.java
+++ back/src/main/java/com/ssafy/banchic/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40c5e5812df8aa557e9c7d16892349cde0cb81bf075a9c9c98eeb5feedbaaa0b
-size 14955
+oid sha256:de0a5387dbd967f0975a2049e55f697a1974125e4648ea29af6ee7a4b8be93ab
+size 14991
