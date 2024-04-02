9e8336ed34f303eed40bcbdad41eaec423da01e2
이호진 <skwo777@naver.com>
2024-04-02 12:27:12 +0900

    refactor: 변수명 수정
    Former-commit-id: 8ea028e73005f4a1925804ed65b924ef848f74b2

--- front/src/components/pages/survey/surveyResult.tsx
+++ front/src/components/pages/survey/surveyResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b43c488a1703c78c316cd59497e0eb009ad81b6cc92de0f3896ae9d28b3913de
-size 5033
+oid sha256:befcdb0802a58eb43d90db5ca36968f0f9cd6b2ff44c20e87be5b0782a1c83f8
+size 5054
