e865f8ed7358a28b2e8ca4d6b1d1b6f758e7285c
백성욱 <b9s2w6@naver.com>
2024-04-02 15:03:55 +0900

    ci: db라이브러리 제거
    Former-commit-id: af3626790cdd1873a2aa3d658f7a95751a5e33a7 [formerly b517069b69580c49f1f249b72153a5a196929417]
    Former-commit-id: 6d4ecebf6ccff71f9fcf8b4f70f57ed0603d2025

--- ai/fastapi/recommend.py
+++ ai/fastapi/recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c815d087b0cbd6ab41cd68f127c80a969bd174e9d395649401a68e388c347606
-size 6440
+oid sha256:21f49b733d9bfc890d6bc7e1320c7cf9a1207793b150df0b993a3d3adbaecfef
+size 6427
