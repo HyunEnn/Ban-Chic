4380d719db66269ac1b0413cb0d6cd76002027f7
이호진 <skwo777@naver.com>
2024-04-02 17:13:51 +0900

    fix: null 처리
    Former-commit-id: 55b2bee66b0a9c66aefaaf1bf0be8158e32efa99

--- front/src/components/pages/recommend/index.tsx
+++ front/src/components/pages/recommend/index.tsx
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4b02d13e63de1c0d8ee84c6bfd5c80939650794da95515c71b17ba6d5d06485c
-size 233
+import CategorySearch from "./CategorySearch";
+import RecommendList from "./RecommendList";
+
+function RecommendPage() {
+  return (
+    <>
+      <RecommendList />
+      <CategorySearch />
+    </>
+  );
+}
+
+export default RecommendPage;
