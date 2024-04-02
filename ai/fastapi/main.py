834aa83f1251f5e1cfb96163215dc781f86398ea
백성욱 <b9s2w6@naver.com>
2024-04-02 15:53:37 +0900

    fix: main.py 수정
    Former-commit-id: 94986020236d95b6b6d2044a310da0f067c3c5a1 [formerly bc6bc76a56c761a92568d069bbc21e2dc9d13d25]
    Former-commit-id: eae0d9f6965f7af378278ab39c6f45aeca35ed96

--- ai/fastapi/main.py
+++ ai/fastapi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8fa14439f8272bcd437b2010f8366da6723b6c7f2dedae06eaba3db98fb0de3
-size 1055
+oid sha256:bf164493cc647f0878fa0f38c91f4d7197559daa7adfa2fc5424e68e1263ef78
+size 1072
