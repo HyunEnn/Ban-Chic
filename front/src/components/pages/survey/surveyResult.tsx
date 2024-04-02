d0166687c22cc85126e61e084cc14e3b93c819ed
이호진 <skwo777@naver.com>
2024-04-02 17:13:51 +0900

    fix: null 처리
    Former-commit-id: 55b2bee66b0a9c66aefaaf1bf0be8158e32efa99

--- front/src/components/pages/survey/surveyResult.tsx
+++ front/src/components/pages/survey/surveyResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a3988e74273ac7e6ed16c72284008891b01bcbdaf1ed2eb2419e312ddd9c40f
-size 5070
+oid sha256:b0efca018c38537bb5ca4fd4008ff455bf8331e69393339e922cebe61904062d
+size 5130
