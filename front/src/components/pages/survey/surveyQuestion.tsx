ce48686c19ee33863f2106c5f5d9a03d88127d17
이호진 <skwo777@naver.com>
2024-04-02 16:20:42 +0900

    feat: 질문 기반 추천 페이지 구현
    #S10P22B109-323
    
    
    Former-commit-id: 9c453efcf13ebab2ebd65de160e2c1d30be3ac0b

--- front/src/components/pages/survey/surveyQuestion.tsx
+++ front/src/components/pages/survey/surveyQuestion.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ac3d499a846e963166c8d70620bfd1f157d85122544f4ab9c63984be6568cee
-size 3801
+oid sha256:8f5381188d553f509ee914848b1cc9e5e5d9fe990d85e80af8635dc9286a5336
+size 20117
