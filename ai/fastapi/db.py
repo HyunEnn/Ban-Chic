87ecd89c826553ff2ad939bf8e55b6f70fd208a3
백성욱 <b9s2w6@naver.com>
2024-04-02 09:26:43 +0900

    fix: CF 알고리즘 수정
    Former-commit-id: 0828bb92e4d0526724442fb1daa029a96fc5cbc6 [formerly 203477448ef0d774488459a2b8c4b9259ac867c5]
    Former-commit-id: d7f4712327103867d8fc2ebda96a4794d3d8609e

---
+++ ai/fastapi/db.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ecfea2ada331da84bf5b798ae3d9c02bc15b291b48e65812d458dbe1435de27b
+size 744
---
+++ ai/fastapi/model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3aa2d6b57b743391473db20484677e4e968f6f399cfeec20c2ef8aff55571243
+size 1016
