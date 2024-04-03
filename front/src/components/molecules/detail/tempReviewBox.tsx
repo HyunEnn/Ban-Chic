998a990c13a8d59d4a77e97d05c50a4a9b21804e
이호진 <skwo777@naver.com>
2024-04-03 17:27:24 +0900

    fix: 질문 기반 추천 수정
    Former-commit-id: f7009d801269380ee3baa971a1cae6be564e9779

--- front/src/components/molecules/detail/tempReviewBox.tsx
+++ front/src/components/molecules/detail/tempReviewBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dd9f4ec898dd7686aba761f38a4b9e66487225261ba9093ffe762f62d7fab24
-size 5058
+oid sha256:7a53d86ea30c71d75d0946f5c2daa499e19704829a5f2acd587890c1bde087a5
+size 5072
