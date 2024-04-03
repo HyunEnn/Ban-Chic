c5daa34c5be69f8b22da0af53804ade39a37441e
이호진 <skwo777@naver.com>
2024-04-03 15:27:05 +0900

    refactor: 버튼 인터랙션 변경
    Former-commit-id: 0c036bf0950d686c8c6fc9447d2cb4f977d7b282

--- front/src/components/atoms/modal/Modal.tsx
+++ front/src/components/atoms/modal/Modal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:248876ef55c00e026fda9210f87392922f9f664b16579ef0181595c06187ae20
-size 2842
+oid sha256:fb440f7060dcfc158b642fbc44c83ddac3b8a9438fd3cae81b8eca19b58620c7
+size 2997
