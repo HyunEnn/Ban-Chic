e9188116f8405176448d5916b0d8e8508c3ae391
이호진 <skwo777@naver.com>
2024-04-04 04:39:59 +0900

    style: css 설정
    Former-commit-id: ae68f504900bf6bf67449fe54655a3d37e11a28b

--- front/src/components/pages/recommend/index.tsx
+++ front/src/components/pages/recommend/index.tsx
@@ -1,13 +1,3 @@
-import CategorySearch from "./CategorySearch";
-import RecommendList from "./RecommendList";
-
-function RecommendPage() {
-  return (
-    <>
-      <RecommendList />
-      <CategorySearch />
-    </>
-  );
-}
-
-export default RecommendPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb1c1bd080d0ed29e5adcab3a801b9474c2119569feef80981edc469cfcf5e19
+size 244
