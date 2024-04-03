619ad5ccbe46c353b59b50367b06423972693c8f
이호진 <skwo777@naver.com>
2024-04-04 02:24:22 +0900

    feat: 형용사 선별 로직 추가
    Former-commit-id: 9f5556565f85ab5b89a5b389e6b20a647ee80588

--- front/src/components/pages/survey/surveyselect.tsx
+++ front/src/components/pages/survey/surveyselect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee12949ed7c4d321f17c6777fec1011fbfa3745436293d5fe8c379d771d3520a
-size 4803
+oid sha256:f12e037a08d637489f1cfb3521287242957b39209c0baf9a6a62197a6ce8f9a1
+size 5643
