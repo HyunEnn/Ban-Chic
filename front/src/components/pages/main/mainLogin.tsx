132f16adbd98531da8e6f090c1a5954581840175
이호진 <skwo777@naver.com>
2024-04-04 06:18:45 +0900

    style: css 수정
    Former-commit-id: 77fe05ce06515a21b2869497c5f03bacbfe2dc89

--- front/src/components/pages/main/mainLogin.tsx
+++ front/src/components/pages/main/mainLogin.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7907a0d73ff7f480cea0334be8ec9ddf501d354f4d2f8f2285bdc1802a6441af
-size 2992
+oid sha256:bf035ef3f0d63d569bc65fea0cb702ee9aff7a437edbdb0de243c1e80cf81320
+size 3783
