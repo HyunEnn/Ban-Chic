42091e08139e98dc801ecdd076e760949d16b273
이호진 <skwo777@naver.com>
2024-04-04 00:38:45 +0900

    fix: 미사용 코드 제거
    Former-commit-id: 7cab8c1a9f648a713b5716e770ea744d18e2afc1

--- front/src/components/pages/mainSample/mainSample.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:012fd36e4458cb43448fe7eb19c6315f684c11038531fbe149e44f950509ed89
-size 3256
--- front/src/components/pages/perfumeDetail/indexcopy.tsx
+++ front/src/components/pages/perfumeDetail/indexcopy.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ee0e31a0bc949b5f9889e4ad0ef3081c75ad4dcbf4fedf0b75fb8b7dad4df3e
-size 11766
+oid sha256:3a0aae8e7a7790b510b24081ca7fb47e5959f2739ab3e7bdcdc3343bf4b86d23
+size 11631
