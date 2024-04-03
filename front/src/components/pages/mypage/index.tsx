e8be46f0d2cf270d82febf04b53d24a124ca357d
이호진 <skwo777@naver.com>
2024-04-03 15:26:18 +0900

    feat: 마이페이지 기본 상태 표시 추가
    Former-commit-id: f47b347c742277b6d9f3e1972416eec415c5fd3e

--- front/src/components/pages/mypage/index.tsx
+++ front/src/components/pages/mypage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:197dc32984ade38eee77d3f2b33fa123030854dcc899a95f9a89336c1c7ec50f
-size 5611
+oid sha256:5c330dce83d197c5d35f6697b8ec6c692c00ff2064ae456b02dc048c7f12dd0b
+size 6317
