cde816ab3ee1db6ecba3e5fbac3454283d0519c3
이호진 <skwo777@naver.com>
2024-04-04 04:39:16 +0900

    refactor: 닉네임 추출
    Former-commit-id: e8fd0aac87e72df4ec8f8b52e45396dbfc2096c2

--- front/src/hooks/auth/useLogout.tsx
+++ front/src/hooks/auth/useLogout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e983883e5d671641bace47dca0159d1b9825f10780008ee9e0f8c773b9947cb
-size 1103
+oid sha256:803fed4e79c279f992a2ea8066e37762598ed3ee4258de98da4b827e5279045c
+size 1144
