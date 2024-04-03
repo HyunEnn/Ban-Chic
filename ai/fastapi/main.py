7da888473fc7e15da6889769bfdefa6a2c589b96
백성욱 <b9s2w6@naver.com>
2024-04-03 17:26:22 +0900

    perf: 모델성능개선
    Former-commit-id: a8d91843d3be5b9617728c8672a15f61ab04efc4 [formerly a23cb05eec2dde80c2e1197bf0f61596a27b613b]
    Former-commit-id: 7d7234ab3a60b4ff15e43e16d790df220d5effe0

--- ai/fastapi/main.py
+++ ai/fastapi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2be5b2c6c6fe09851515f60cc43fa02a2b85631a4126c81d7b9eaf51502da259
-size 1096
+oid sha256:40268ab3a438d4671a23d338b0bd0fa32c549ab77384a61214d1b6dd8b7a731e
+size 1109
