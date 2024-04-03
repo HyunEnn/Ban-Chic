ef8edee697b8d71f4fb3409086b648546c45fc41
이호진 <skwo777@naver.com>
2024-04-03 12:42:52 +0900

    design: 디자인 수정
    #S10P22B109-324
    
    
    Former-commit-id: 1b36b9044c1775143517c1b2cd7cb938f4e54ae1

--- front/src/components/molecules/detail/perfume.tsx
+++ front/src/components/molecules/detail/perfume.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc5de21632d9834d40367252a1d9ae744bfa0ce0ff634cb24bf75e05b5a86f3c
-size 2106
+oid sha256:f69d3f7484fda2e94e4e7b74b7c2e16ceb3302e5103ed865408fe18fa91f72e9
+size 2271
