78f24fae4c7c3447716d0433eb924ec5d5df1233
이호진 <skwo777@naver.com>
2024-04-04 02:31:48 +0900

    style: css 수정
    Former-commit-id: e33c3c1196fba5254ea853e831ca7ce4f7f4a98f

--- front/src/components/molecules/mypage/persuitCard.tsx
+++ front/src/components/molecules/mypage/persuitCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd37e86d2d420c6ed9bd537f7ff5065bce17be07d91275c44cc91f357140cf0b
-size 1106
+oid sha256:491c439471298d64ee1326077a7808b9760304b99bfa63a6d1c53cc4b3071342
+size 1156
