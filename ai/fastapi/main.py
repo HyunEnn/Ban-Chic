dc863a483abec7a8fb0f4fab0fa6d184263f7997
백성욱 <b9s2w6@naver.com>
2024-04-03 10:33:27 +0900

    fix: 이미지 추천 코드 수정
    Former-commit-id: c5c54e585d2f8bc4710f68d8696eeba5997efaa3 [formerly a4615a719ac348d5f7e1a6df8dcad2fe70bb5728]
    Former-commit-id: c6356f09a7aab4c35ac926c7444a216cfa0cf77d

--- ai/fastapi/main.py
+++ ai/fastapi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd00c4d6328cf5236226123182e52ef26c4be58efc30e73eb490d3616a8c8fc3
-size 1088
+oid sha256:2be5b2c6c6fe09851515f60cc43fa02a2b85631a4126c81d7b9eaf51502da259
+size 1096
