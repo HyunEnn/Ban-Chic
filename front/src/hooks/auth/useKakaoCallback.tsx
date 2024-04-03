e0ba05da9a36ac8a85225e94f80d234ba8981f3e
이호진 <skwo777@naver.com>
2024-04-04 04:39:16 +0900

    refactor: 닉네임 추출
    Former-commit-id: e8fd0aac87e72df4ec8f8b52e45396dbfc2096c2

--- front/src/hooks/auth/useKakaoCallback.tsx
+++ front/src/hooks/auth/useKakaoCallback.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c02ebc901b2dfd0b5354340c3aa812d22bf6d4d658d453612484ad8902a4642
-size 1378
+oid sha256:73ce5fbab72617bb6c8e955cb119bbc1161b6d66bab920405ae5c069329f1c67
+size 1448
