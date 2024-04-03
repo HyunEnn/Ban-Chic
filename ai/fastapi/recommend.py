7ec0797bde56e1e9092ce4253b5bdd5031eb9924
백성욱 <b9s2w6@naver.com>
2024-04-03 19:01:28 +0900

    fix:model 변경
    Former-commit-id: 931b471bf2a2f0729bc89a95387368af39ee139b [formerly 6342757b2ee32fd5a2e39134a08b3aeac2cd3572]
    Former-commit-id: dca09804d612bebbaa3b9660357d6c8417957c81

--- ai/fastapi/recommend.py
+++ ai/fastapi/recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e05b287cefa769861e6a81204ea939dac7567bbb18bfa2d35c11b0f99cef2a0
-size 6649
+oid sha256:e6041d8778bef0c01edea06c8b5809631e3f7b2343572b74ddd51b769c466623
+size 6619
