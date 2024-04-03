fb91a93e2738ba0be7c87c62620290eed17f77cf
이호진 <skwo777@naver.com>
2024-04-03 12:41:49 +0900

    fix: 빌드 에러 수정
    Former-commit-id: c96a0e4a12ae28da8cb9619f85692dc7c45c9c4c

--- front/src/components/atoms/modalForm/ModalUpdateForm.tsx
+++ front/src/components/atoms/modalForm/ModalUpdateForm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:711fad218d70f190a0ad84e99940c0ee21930dc30c0b48ed2af91788e4c3d29d
-size 3887
+oid sha256:86395fa10cbe0cd518f421c41d9a700b859acc232c1477940a3bbc1479e4b778
+size 3921
