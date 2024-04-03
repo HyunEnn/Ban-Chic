2af25e0a1fb640e5bbbd5c0a9fa30261b47e1af0
김현준 <denny10002@naver.com>
2024-04-03 22:27:04 +0900

    fix: map search img 수정
    Former-commit-id: 45cefa7b7cd9cb3c9a662378b77afb115c4141d9

--- front/src/components/atoms/kakaomap/SearchPlaces.tsx
+++ front/src/components/atoms/kakaomap/SearchPlaces.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00f7ee2e6b9aec5ab5c31e2e2711e4a3c3df02aee0401c4411dea92ebb1fc043
-size 1142
+oid sha256:c499149273c0e4b8cf3f12ef8add881fca1486805004a196ad4ded399855c49e
+size 1135
