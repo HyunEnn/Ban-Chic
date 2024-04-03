0a0376df9eaa982a00c02a140e74c7e8f19b108e
이호진 <skwo777@naver.com>
2024-04-03 15:40:29 +0900

    feat: 마이페이지 작성리뷰 표시
    Former-commit-id: 48b0020273fc66a8afdf9af975ee62674d94cd65

--- front/src/components/pages/mypage/index.tsx
+++ front/src/components/pages/mypage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c330dce83d197c5d35f6697b8ec6c692c00ff2064ae456b02dc048c7f12dd0b
-size 6317
+oid sha256:0815f0d4eb3ff138d8b76f1bf6a9b38dfc75f3e486beec1c21f011748209e7a5
+size 6650
